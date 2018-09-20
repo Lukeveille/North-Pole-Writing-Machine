e2a779972203be6383b9a59a073b615e1086d77d
Lukeveille <luke@lukeleveille.com>
2018-09-20 14:47:10 -0400

    Initial commit

---
+++ dialogue.rb
@@ -0,0 +1,13 @@
+require "erb"
+
+line_1 = "Bill:   That will cost you $8.75 plus taxes, ma'am."
+line_2 = "Pritma: How much is it with taxes?"
+line_3 = "Bill:  I don't know! I'm not very good at math."
+
+puts
+puts line_1
+puts
+puts line_2
+puts
+puts line_3
+puts
